1c5495bcf00dd20e0df1772030dde15fc0712163
kin0man <ilaki@LAPTOP.>
2025-05-19 14:52:19 +0300

    end

--- integral.c
+++ integral.c
@@ -3,31 +3,48 @@
 #include "main.h"
 #include <float.h>
 
-double interval(double a, double b, double fa, double fm, double fb) {
-    return (b - a) / 6 * (fa + 4 * fm + fb);
-}
 
-double recursive_integral(func f, double a, double b, double fa, double fm, double fb, double eps){
-    double m = (a + b) / 2;
-    double m1 = (a + m) / 2;
-    double m2 = (m + b) / 2;
-    double fm1 = f(m1);
-    double fm2 = f(m2);
-    double s_prev = interval(a, b, fa, fm, fb);
-    double s_left = interval(a, m, fa, fm1, fm);
-    double s_right = interval(m, b, fm, fm2, fb);
-    double s_current = s_left + s_right;
-    if (fabs(s_prev - s_current) < 15 * eps)
-        return s_current;
-    return recursive_integral(f, a, m, fa, fm1, fm, eps) +
-           recursive_integral(f, m, b, fm, fm2, fb, eps);
-}
-
-double integral(double (*f)(double), double a, double b, double eps) {
-    double fa = f(a);
-    double fb = f(b);
-    double fm = f((a + b) / 2);
-    return recursive_integral(f, a, b, fa, fm, fb, eps);
+double integral(func f, double a, double b, double eps) {
+    int n = 1;
+    double start_I;
+    double h = (b - a) / (2 * n);
+    double even_sum = 0.0;
+    double x = a + 2 * h;
+    for (int i = 2; i < 2 * n; i += 2){
+        even_sum += f(x);
+        x += h * 2;
+    }
+    even_sum *= 2;
+    x = a + h;
+    double odd_sum = 0.0;
+    for (int i = 1; i < 2 * n; i += 2){
+        odd_sum += f(x);
+        x += h * 2;
+    }
+    odd_sum *= 4;
+    start_I = (f(a) + f(b) + odd_sum + even_sum) * h / 3;
+    double I = start_I;
+    odd_sum /= 4;
+    even_sum /= 2;
+    do
+    {   
+        n *= 2;
+        start_I = I;
+        double H = (b - a) / (2 * n);
+        double even_sum_ = even_sum + odd_sum;
+        even_sum_ *= 2;
+        double X = a + H;
+        double odd_sum_ = 0.0;
+        for (int i = 1; i < 2 * n; i += 2){
+            odd_sum_ += f(X);
+            X += H * 2;
+        }
+        odd_sum_ *= 4;
+        I = (f(a) + f(b) + odd_sum_ + even_sum_) * H / 3;
+        even_sum = even_sum_ / 2;
+        odd_sum = odd_sum_ / 4;
+    } while (fabs(I - start_I) > eps / 15);
+    return I;
 }
 
 // double integral(func f, double a, double b, double eps){
